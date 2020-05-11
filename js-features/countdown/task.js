9a7be13830385175eb519101c74af96461d55fb7
Svetlana Ryabova <ganyim@inbox.ru>
2020-05-11 22:41:16 +0500

    11/05/20

---
+++ js-features/countdown/task.js
@@ -0,0 +1,76 @@
+'use strict'
+
+//#1
+// const getCounter = () => {
+//     const timerText = document.getElementById('timer')
+//     let timerValue = timerText.textContent
+//     let firstTimerValue = timerValue
+//     return setInterval(() => {
+//         --timerValue
+//         timerText.textContent = timerValue
+//         if (timerValue == 0) {
+//             alert('Вы победили в конкурсе!')
+//             timerValue = firstTimerValue
+//         }
+//     }, 100)
+// }
+//getcounter()
+
+
+//#2
+function getCounter(hour, minute, sec, winform) {
+    setInterval(() => {
+        if (sec > 0) {
+            --sec
+            winform.textContent = (`${hour}: ${minute}: ${sec}`)
+            console.log(winform.textContent)
+        } 
+    }, 500)
+    
+}
+
+
+function timer() {
+ 
+    let timerText = document.getElementById('timer')  //переменная с текстом
+    let date = new Date(2020, 9, 5, 0, 1, 3)           // вносим новые данные
+    timerText.textContent = (`${date.getHours()}: ${date.getMinutes()}: ${date.getSeconds()}`)  //меняем данные в html
+    let sumTimerArray = 0                          // переменная для проверки времени
+    let timerArray = [date.getHours(), date.getMinutes(), date.getSeconds()]
+    sumTimerArray = timerArray[0] * 3600 + timerArray[1] * 60 + timerArray[2]
+    console.log(sumTimerArray)
+
+    do {
+        if ((timerArray[2] <= 59) && (timerArray[2] > 0)) {
+            getCounter(timerArray[0], timerArray[1], timerArray[2], timerText)
+            timerArray[2] = 0
+            sumTimerArray = timerArray[0] * 3600 + timerArray[1] * 60
+            console.log(timerArray + '----' + sumTimerArray)
+        } else if (sumTimerArray < 3600) {
+            --timerArray[1]
+            timerArray[2] = 59
+            timerText.textContent = (`${timerArray[0]}: ${timerArray[1]}: ${timerArray[2]}`)
+            sumTimerArray = timerArray[0] * 3600 + timerArray[1] * 60
+            console.log(timerArray + '----' + sumTimerArray)
+        } else {
+            --timerArray[0]
+            timerArray[1] = 58
+            timerArray[2] = 59
+            timerText.textContent = (`${timerArray[0]}: ${timerArray[1]}: ${timerArray[2]}`)
+            sumTimerArray = timerArray[0] * 3600 + timerArray[1] * 60
+            console.log(timerArray + '----' + sumTimerArray)
+        }
+    } while (sumTimerArray > 0)
+}
+
+timer()
+
+
+
+
+
+
+
+
+
+
