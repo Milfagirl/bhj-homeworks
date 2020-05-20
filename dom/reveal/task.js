dd87d40d5ab019b8ab523a86c6b6445f488459eb
Svetlana Ryabova <ganyim@inbox.ru>
2020-05-21 00:44:18 +0500

    21/05/20

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,24 +1,14 @@
 const reveal = document.querySelectorAll('.reveal')
-//console.log(reveal)
-function getTop(array) {
-    let arrPosY = []
-    for (let i of array) {
-        arrPosY.push([(i.getBoundingClientRect()).top, (i.getBoundingClientRect()).bottom])
-    }
-    return arrPosY
-}
-
-const posY = getTop(reveal)
-//console.log(posY)
 
 window.addEventListener('scroll', () => {
-    //console.log(scrollY)
-    for (let i in posY) {
-        // console.log(i+'   '+scrollY+'   '+posY)
-        if ((scrollY >= posY[i][0]) && (scrollY <= posY[i][1])) {
-            // console.log(reveal[i])
-            reveal[i].classList.toggle('reveal_active')
+    reveal.forEach(arr => {
+
+        if (arr.getBoundingClientRect().top * 0.8 < window.innerHeight) {
+            arr.classList.add('reveal_active')
         }
-    }
+        if ((arr.getBoundingClientRect().top * 0.8 < 0) || (arr.getBoundingClientRect().top * 1.2 > window.innerHeight)) {
+            arr.classList.remove('reveal_active')
+        }
+    })
 
 })
