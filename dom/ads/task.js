a0d416295601b77c4354563e9db71ff86088008c
Svetlana Ryabova <ganyim@inbox.ru>
2020-05-28 21:02:16 +0300

    28/05/20

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,39 +1,56 @@
 let rotatorCase = document.querySelector('.rotator').firstElementChild
 let datacolor
-let dataspeed
 
-let timer = setInterval(() => {
+
+function getChange() {
     if (!!rotatorCase.nextElementSibling) {
         datacolor = rotatorCase.dataset.color
-        dataspeed = rotatorCase.dataset.speed - 1000
-        setTimeout(() => {
-            rotatorCase.classList.remove('rotator__case_active')
-            rotatorCase.nextElementSibling.style.color = datacolor
-            rotatorCase.nextElementSibling.classList.add('rotator__case_active')
-            rotatorCase = rotatorCase.nextElementSibling
-            console.log(rotatorCase)
-        }, dataspeed)
-
-    } else {
-
+        rotatorCase.classList.remove('rotator__case_active')
+        rotatorCase.nextElementSibling.style.color = datacolor
+        rotatorCase.nextElementSibling.classList.add('rotator__case_active')
+        rotatorCase = rotatorCase.nextElementSibling
+    } 
+    else {
         rotatorCase.classList.remove('rotator__case_active')
         rotatorCase = document.querySelector('.rotator').firstElementChild
         datacolor = rotatorCase.dataset.color
-        dataspeed = rotatorCase.dataset.speed - 1000
-        setTimeout(() => {
-            rotatorCase.style.color = datacolor
-            rotatorCase.classList.add('rotator__case_active')
-        }, dataspeed)
-
+        rotatorCase.style.color = datacolor
+        rotatorCase.classList.add('rotator__case_active')
     }
-
-}, 1000)
-
-
-
-document.onclick = () => {
-    clearInterval(timer)
+    setTimeout(getChange, 1000)
 }
+
+getChange()
+
+// let timer = setInterval(() => {
+//     if (!!rotatorCase.nextElementSibling) {
+//         datacolor = rotatorCase.dataset.color
+//         dataspeed = rotatorCase.dataset.speed - 1000
+//         setTimeout(() => {
+//             rotatorCase.classList.remove('rotator__case_active')
+//             rotatorCase.nextElementSibling.style.color = datacolor
+//             rotatorCase.nextElementSibling.classList.add('rotator__case_active')
+//             rotatorCase = rotatorCase.nextElementSibling
+//             console.log(rotatorCase)
+//         }, dataspeed)
+
+//     } else {
+
+//         rotatorCase.classList.remove('rotator__case_active')
+//         rotatorCase = document.querySelector('.rotator').firstElementChild
+//         datacolor = rotatorCase.dataset.color
+//         dataspeed = rotatorCase.dataset.speed - 1000
+//         setTimeout(() => {
+//             rotatorCase.style.color = datacolor
+//             rotatorCase.classList.add('rotator__case_active')
+//         }, dataspeed)
+
+//     }
+
+// }, 1000)
+// document.onclick = () => {
+//     clearInterval(timer)
+// }
 
 
 
